216cd023ea5a8575eaaca6d45d42717204707f36
zircod <zircod@mail.ru>
2022-08-27 19:18:55 +0300

    #lesson-3 add basket

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductList {
-  constructor(container= '.products') {
+  constructor(container= '.products', url = '/catalogData.json') {
     this.container = container;
+    this.url = url;
     this.goods =[];
-    this._fetchProducts();
-    this.render();
+    this.init();
+    this.getProducts()
+      .then(data => {
+        this.goods = data;
+        console.log(data);
+        this.render();
+      })
 
   }
 
-  _fetchProducts() {
-    this.goods = [
-      {id: 1, title: 'Notebook', price: 2000},
-      {id: 2, title: 'Mouse', price: 20},
-      {id: 3, title: 'Keyboard', price: 200},
-      {id: 4, title: 'Gamepad', price: 50},
-    ];
+  getProducts() {
+    return fetch(`${API + this.url}`)
+      .then(result => result.json())
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
-    document.querySelector('.products').innerHTML = this.goods.map(item => {
+    document.querySelector(this.container).innerHTML = this.goods.map(item => {
       const goodItem = new ProductItem(item);
       return goodItem.render();
     }).join('');
@@ -27,47 +34,79 @@ class ProductList {
     return this.goods.reduce((accum, item ) => accum + item.price, 0)
   }
 
+  init() {
+  //  return false;
+  }
 }
 
 class ProductItem {
   constructor(product, img= 'https://i.ibb.co/bvjZbP9/black-friday-elements-assortment.jpg') {
-    this.title = product.title;
-    this.id = product.id;
+    this.title = product.product_name;
+    this.id = product.id_product;
     this.price = product.price;
     this.img = img;
   }
 
   render() {
-    return`<div class="product-item">
+    return`<div class="product-item" data-id="${this.id}">
               <img src="${this.img}" alt="img">
               <h3>${this.title}</h3>
               <p>${this.price}</p>
-              <button class="buy-btn">Купить</button>
+              <button class="buy-btn btn">Купить</button>
             </div>
     `;
   }
 }
 
+class Basket extends ProductList {
+  constructor(container= '.basket', url='/getBasket.json') {
+    super(container, url)
+    this.item = [];
+    this.getProducts()
+      .then(data => {
+        this.item = data.contents;
+        console.log(data);
+        this.render();
+      })
+  }
+
+  render() {
+    document.querySelector(this.container).innerHTML = this.item.map(item => {
+      const basketItem = new ItemInBasket(item);
+      return basketItem.render();
+    }).join('');
+  }
+
+  init() {
+    document.querySelector('.btn-cart').addEventListener('click', () => {
+      document.querySelector(this.container).classList.toggle('basket-active');
+    });
+  }
+}
+
+class ItemInBasket extends ProductItem {
+  constructor(product) {
+    super(product)
+    this.quantity = product.quantity;
+  }
+
+  render() {
+    return `
+      <div class="basket-item" data-id="${this.id}">
+        <p>${this.title}</p>
+        <p>${this.price} $ за 1шт.</p>
+        <p>${this.quantity} шт.</p>
+        <div class="bnt-wrp">
+            <button class="delete-btn btn" data-id="${this.id_product}">x</button>
+        </div>
+      </div>
+     
+    `
+  }
+}
+
 const list = new ProductList();
-console.log(list.calcSum());
+let products = new Basket();
+//console.log(list.calcSum());
 
-class Basket {
-  getTotalCount(){
 
-  }
-  getTotalPrice(){
-
-  }
-  addToCard() {
-
-  }
-  renderProductInBasket() {
-
-  }
-}
-
-class ItemInBasket {
-  renderNewProductInBasket() {
-
-  }
-}
